026a7956c8d467edbcbc26f336385db031366cbc
Rails Abam <abamsunday5@gmail.com>
2022-03-18 01:58:02 +0100

    commiting changes

---
+++ MySideOfTheProject/script.js
@@ -0,0 +1,15 @@
+
+let javascript = true;
+console.log(javascript)
+
+javascript = "It Fun"
+console.log(javascript) //changing the value of the value
+
+const now = 2037;
+const railsAge = now - 1993;
+const rubyAge = now - 2017;
+console.log(railsAge, rubyAge);//40, 20
+console.log(railsAge ** 2)//This we are saying railsAge * 4 = 1936
+console.log( rubyAge * 2);
+// railsAge ** 2 means railsAge to the power of 2(2 * 2)
+
