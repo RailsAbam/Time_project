1eaa96ebd7c1a1a8f0f2aa2d4127072335793766
Rails Abam <abamsunday5@gmail.com>
2022-03-18 08:00:52 +0100

    commiting

---
+++ MySideOfTheProject/challenge.js
@@ -0,0 +1,9 @@
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+//Calculating the Height and Mass;
+let Bmi = markMass / markHeight ** 2 ;
+let BmiJohn = johnMass / (johnHeight * johnHeight);
+console.log(Bmi);
+console.log(BmiJohn);
--- MySideOfTheProject/script.js
+++ MySideOfTheProject/script.js
@@ -13,3 +13,16 @@ console.log(railsAge ** 2)//This we are saying railsAge * 4 = 1936
 console.log( rubyAge * 2);
 // railsAge ** 2 means railsAge to the power of 2(2 * 2)
 
+let time = 100;
+time += 10;
+time *= 2; //Basic Operator in javaScript;
+console.log(time);
+console.log(railsAge > rubyAge) 
+//Asking if railsAge is Greater than rubyAge which is true
+console.log(railsAge <= rubyAge);
+//Asking if railsAge is less than rubyAge which is false.
+
+const averageAge = (railsAge + rubyAge) / 2 ;
+// By Using paratesis we can precidence is given to the values in the brackets
+console.log(averageAge);
+
