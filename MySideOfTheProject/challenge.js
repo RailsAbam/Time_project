bc6701495ffe66b3fdc42bce475810677f0dfb45
Rails Abam <abamsunday5@gmail.com>
2022-03-23 03:56:07 +0100

    commiting new files

--- MySideOfTheProject/challenge.js
+++ MySideOfTheProject/challenge.js
@@ -15,3 +15,17 @@ console.log(`Mark BMI is ${markBmi} which is much higher John BMI  ${(BmiJohn)}`
 }else{
     console.log(`Johns's BMI is ${(BmiJohn)} which is lower than Mark ${(markBmi)}`)
 }
+
+
+const inputYear = "1993";
+changeNumber = Number(inputYear);// Changing the string to a Number; 
+console.log(changeNumber + 18);
+//Changing a number to A String 123;
+
+num = 123;
+str = String(num);
+console.log(str);
+
+
+console.log('23' - '10' - 3);
+console.log('22' * 2);
--- MySideOfTheProject/iflsestatement.js
+++ MySideOfTheProject/iflsestatement.js
@@ -18,3 +18,20 @@ if(birthYear <= 2000){
     century = 21;
 }
 console.log(century);
+
+
+const newAge = 18;
+if(newAge === 18) console.log("You just turn adult");4
+
+
+const favorite = Number (prompt("What is your Age"));
+console.log(favorite);
+if(favorite === 29){
+    console.log("Cool you are getting old!!")
+}else if(favorite === 7){
+console.log("7 is cool Number")
+}else{
+    console.log("Number is not 29 or 7")
+}
+
+
