f52ed9bbe97d3099ce691bb0664da76d473426d6
Rails Abam <abamsunday5@gmail.com>
2022-05-20 20:46:49 +0100

    commiting

--- MySideOfTheProject/udacity.js
+++ MySideOfTheProject/udacity.js
@@ -8,3 +8,22 @@ console.log ("$" + total)
 sum = total.toFixed(2);
 console.log(sum);
 
+
+//Checking Balance
+ 
+const checkBalance = true;
+const accountBalance = 20.673032;
+const isActive = true;
+if(checkBalance === true){
+    console.log( accountBalance.toFixed(2) );
+}
+if(isActive === true && accountBalance > 0){
+    console.log("My account is " + "$" + accountBalance.toFixed(2));
+}else if( isActive === true  && accountBalance === 0){
+    console.log("Your account is negative, please contact bank");
+}else if (isActive === false){
+    console.log("Your account is longer active");
+}else{
+    console.log("Thank you.Have a nice day");
+}
+
