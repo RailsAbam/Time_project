83ff714753ba1d4ca4ad2db2f06af26c2b54a206
Rails Abam <abamsunday5@gmail.com>
2022-05-22 11:41:46 +0100

    commiting ice-cream file

--- MySideOfTheProject/IceCream.js
+++ MySideOfTheProject/IceCream.js
@@ -1,13 +1,14 @@
-const flavour = "chocolate ";
-const topping = " bananas";
-const Vessel = "Wafer cone ";
+const flavour = "chocolate";
+const topping = " sprinkles";
+const Vessel = "bowl";
 
-if(topping === " bananas" &&  flavour === "chocolate " && Vessel === "Wafer cone "){
-    conbsole.log("Please check our menu and try again.");
-}else if( flavour === "chocolate" && topping === "peanuts" || Vessel === "Wafer cone "){
-        console.log("Great choice! Your ice cream is at the next window.");
-}else if(flavour === "chocolate " && topping === "sprinkles" || Vessel === "sugar cone"){
-    console.log("Great choice! Your ice cream is at the next window")
-}else if(topping === "sprinkles" && flavour === "chocolat" || Vessel === "bowl"){
+if(topping === " bananas" &&  flavour === "chocolate" ||  Vessel === "Wafer cone "){
     console.log("Please check our menu and try again.");
+}else if( flavour === "chocolate" && topping === "peanuts" || Vessel === "Wafer-cone "){ // changes topping to 
+        console.log("Great choice! Your ice cream is at the next window.");
+}else if(flavour === "chocolate " || topping === "sprinkles" && Vessel === "sugar-cone"){
+    console.log("Great choice! Your ice cream is at the next window Go get it");
+}else if(flavour === "chocolate"  && topping === "sprinkles" || Vessel === "bowl"){
+    console.log("Please check our menu and try again check later.");
 }
+
